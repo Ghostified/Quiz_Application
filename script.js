bf9a7de8d36be3f2771c73169e20db1ab8688c8e
Ghostified <bransonallan@gmail.com>
2024-07-15 16:12:27 +0300

    added oall the first four key questions

--- script.js
+++ script.js
@@ -15,9 +15,26 @@ const questions = [
         answers: [
             { text:"Griffindor", correct: false},
             { text:"Raven Claw", correct: false},
-            { text:"Huffle Puff", correct: false},
-            { text:"Slytherin", correct: true},
+            { text:"Huffle Puff", correct: true},
+            { text:"Slytherin", correct: false},
         ]
-
+    },
+    {
+        question: "Which one is not an ingredient of the polyjuice potion? ",
+        answers: [
+            { text:"LaceWing Flies", correct: false},
+            { text:"Knot Grass", correct: false},
+            { text:"Bezoar", correct: true},
+            { text:"Flux Weed", correct: false},
+        ] 
+    },
+    {
+        question: "Who was the Quidditch captain in Harry's First Year? ",
+        answers: [
+            { text:"Katie Bell", correct: false},
+            { text:"Bill Weasly", correct: false},
+            { text:"Oliver Wood", correct: true},
+            { text:"Mundungus Fletcher", correct: false},
+        ]
     }
 ]
