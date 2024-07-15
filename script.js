983a098a7903203a2d2940e7ab474ff9b8ebad83
Ghostified <bransonallan@gmail.com>
2024-07-15 16:04:25 +0300

    added all the key files

---
+++ script.js
@@ -0,0 +1,23 @@
+//function to set the questions
+const questions = [
+    {
+        question: "Who is the greatest wizard of all time? ",
+        answers: [
+            { text:"Gilderoy Lockhart", correct: false},
+            { text:"Lord Voldemort", correct: false},
+            { text:"Grindewald", correct: false},
+            { text:"Albus Dumbledore", correct: true},
+        ]
+
+    },
+    {
+        question: "Which house was professor sprout the dorm master? ",
+        answers: [
+            { text:"Griffindor", correct: false},
+            { text:"Raven Claw", correct: false},
+            { text:"Huffle Puff", correct: false},
+            { text:"Slytherin", correct: true},
+        ]
+
+    }
+]
